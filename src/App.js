12f5578b84852cee8a20f7c9df24d0ba6dee8d13
Christian Escobar <crs.escobar@gmail.com>
2018-07-16 22:09:53 -0700

    initial implementation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ValidationComponent from './ValidationComponent/ValidationComponent';
+import CharComponent from './CharComponent/CharComponent';
 
 class App extends Component {
   state = {
